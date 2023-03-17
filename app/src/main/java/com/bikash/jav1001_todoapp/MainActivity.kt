5d121abd9309382ca9ba548f0a6764bfe3c9ebe4
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-03-16 23:47:37 -0400

    Created layout for the todo app

---
+++ app/src/main/java/com/bikash/jav1001_todoapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bikash.jav1001_todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
